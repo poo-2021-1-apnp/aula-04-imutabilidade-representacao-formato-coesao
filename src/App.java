81906ead71f5b20083c187450f0689d71faba5e4
profmarciotorres <marcio.torres@riogrande.ifrs.edu.br>
2021-02-04 15:01:41 -0300

    primeiros exemplos

--- src/App.java
+++ src/App.java
@@ -1,7 +1,58 @@
-// Todo projeto possui uma classe Principal, Main ou nome do projeto (como Template.java)
+import java.util.Arrays;
+
 class App {
-  // a classe principal possui o método principal main()
   public static void main(String[] args) {
-    System.out.println("Olá mundo!");
+    // imutabilidade();
+    Color cinza1 = new Color(215, 215, 215);
+    Color cinza2 = new Color(35, 35, 35);
+    Color cinza3 = new Color(215, 215, 215);
+    System.out.println(cinza1 == cinza2);
+    System.out.println(cinza2 == cinza3);
+    System.out.println(cinza1 == cinza3);
+
+
+
   }
+
+  static void imutabilidade() {
+    System.out.println("Olá mundo!");
+    // primitivos
+    int i = 2;
+    double d = 2.2;
+    boolean b = true;
+    final float f = 3.5F;
+    i = 5;
+    // f = 78; //símbolo f não pode ser reatribuído, é constante
+    // arrays em Java são objetos mutáveis
+    final int[] idades = new int[3];
+    // o final torna o símbolo idades não reatribuível,
+    // mas não torna o array imutável!
+    System.out.println(Arrays.toString(idades));
+    idades[0] = 15;
+    idades[1] = 25;
+    idades[2] = 35;
+    System.out.println(Arrays.toString(idades));
+    idades[2] = 55;
+    System.out.println(Arrays.toString(idades));
+    // = new int[10];
+    // Strings em Java (e muitas outras linguagem) é imutável
+    final String nome = "Marcio";
+    System.out.println(nome);
+    String nova = nome.toUpperCase();
+    System.out.println(nova);
+    System.out.println(nome);
+    zeraArray(idades);
+    System.out.println(Arrays.toString(idades));
+    maiusculas(nome);
+    System.out.println(nome);
+  }
+
+  static void zeraArray(int[] array) {
+    for (int i = 0; i < array.length; i++) array[i] = 0;
+  }
+
+  static void maiusculas(String s) {
+    s.toUpperCase();
+  }
+
 }
---
+++ src/Color.java
@@ -0,0 +1,10 @@
+public class Color {
+  public final int red;
+  public final int green;
+  public final int blue;
+  Color(int red, int green, int blue) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+}
